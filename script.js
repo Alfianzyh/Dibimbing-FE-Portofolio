015998da75a02921b6c0402a4879fed3f86f2044
YaXioz <alfianzyh@gmail.com>
2025-07-20 19:47:46 +0700

    update file

---
+++ script.js
@@ -0,0 +1,14 @@
+// Animasi muncul saat scroll
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+fadeElements.forEach(el => observer.observe(el));
